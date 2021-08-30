9019c09521d7077fa62fdaa620f5048eb81ed56e
Zhang-Haoran <haoranzhang001@gmail.com>
2021-08-30 21:24:19 +1000

    game board layout init

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // css class for different card image
 const CARD_TECHS = [
-  'html5',
-  'css3',
-  'js',
-  'sass',
-  'nodejs',
-  'react',
-  'linkedin',
-  'heroku',
-  'github',
-  'aws'
-];
+  "html5",
+  "css3",
+  "js",
+  "sass",
+  "nodejs",
+  "react",
+  "linkedin",
+  "heroku",
+  "github",
+  "aws",
+]
 
 // only list out some of the properties,
 // add more when needed
@@ -22,20 +22,77 @@ const game = {
   scoreDisplay: null,
   levelDisplay: null,
   timerInterval: null,
-  startButton: null
+  startButton: null,
   // and much more
-};
+}
+// register DOM element for further using
+const DOMcontrol = {
+  levelDisplay: document.querySelector(".game-stats__level--value"),
+  scoreDisplay: document.querySelector(".game-stats__score--value"),
+  startButton: document.querySelector(".game-stats__button"),
+  timerDisplay: document.querySelector(".game-timer"),
+  timeDisplayBar: document.querySelector(".game-timer__bar"),
+  gameBoard: document.querySelector(".game-board"),
+  gameInstruction: document.querySelector(".game-instruction"),
+}
 
-setGame();
+setGame()
 
 /*******************************************
 /     game process
 /******************************************/
 function setGame() {
   // register any element in your game object
+  bindStartButton()
 }
 
-function startGame() {}
+// set Game board
+function setGameBoard() {
+  // set game level based on current level
+  game.levelDisplay = game.level
+  // remove game instructions
+  DOMcontrol.gameInstruction.remove()
+  // set cards number and layout columns
+  const setCard = {
+    // level 1 has 4 cards
+    1: {
+      cardNum: 4,
+      boardColumn: 2,
+    },
+    // level 2 has 16 cards
+    2: {
+      cardNum: 16,
+      boardColumn: 4,
+    },
+    // level 3 has 36 cards
+    3: {
+      cardNum: 36,
+      boardColumn: 6,
+    },
+  }
+  // html for each card element
+  const cardElmentHtml = `
+  <div class='card'>
+  <div class='card__face card__face--front'/>
+  <div class='card__face card__face--back'/>
+  </div>`
+  // set game level display text
+  DOMcontrol.levelDisplay.text = game.levelDisplay
+  // set gameboard css to grid layout
+  DOMcontrol.gameBoard.style.cssText += `
+  grid-template-columns: repeat(${setCard[game.levelDisplay].boardColumn}, 1fr)`
+  // set game card display
+  for (let i = 0; i < setCard[game.levelDisplay].cardNum; i++) {
+    //add card element
+    DOMcontrol.gameBoard.innerHTML += cardElmentHtml
+  }
+}
+
+function handleRestart() {}
+
+function startGame() {
+  setGameBoard()
+}
 
 function handleCardFlip() {}
 
@@ -53,7 +110,17 @@ function updateTimerDisplay() {}
 /*******************************************
 /     bindings
 /******************************************/
-function bindStartButton() {}
+function bindStartButton() {
+  DOMcontrol.startButton.addEventListener("click", function () {
+    // actions based on different button value
+    const actions = {
+      null: startGame(),
+      restart: handleRestart(),
+      end: handleGameOver(),
+    }
+    actions[game.startButton]
+  })
+}
 
 function unBindCardClick(card) {}
 
